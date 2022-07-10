07395a0263783af56a72b12bad16ce1cae701fa0
Yashsaraf96 <sarafyash2015@gmail.com>
2022-07-10 18:19:32 +0530

    Added sounds to drum buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -5,6 +5,32 @@ for (var i=0; i<drumButtons.length; i++){
 }
 
 function handleClick() {
-  var audio = new Audio("sounds/tom-1.mp3")
-  audio.play()
+  var buttonName = this.getInnerHTML();
+  var audioPath = "sounds/";
+  switch (buttonName) {
+    case "w":
+      audioPath += "snare.mp3"
+      break;
+    case "a":
+      audioPath += "tom-1.mp3"
+      break;
+    case "s":
+      audioPath += "tom-4.mp3"
+      break;
+    case "d":
+      audioPath += "kick-bass.mp3"
+      break;
+    case "j":
+      audioPath += "tom-3.mp3"
+      break;
+    case "k":
+      audioPath += "tom-2.mp3"
+      break;
+    case "l":
+      audioPath += "crash.mp3"
+      break;
+    default:
+  }
+  var audio = new Audio(audioPath);
+  audio.play();
 }
