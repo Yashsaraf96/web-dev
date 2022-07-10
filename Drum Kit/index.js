1028ccca34a8beb7353270c54ebb9cf0e824bf77
Yashsaraf96 <sarafyash2015@gmail.com>
2022-07-10 18:54:47 +0530

    Added button animation on click and keypress.

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -7,42 +7,53 @@ for (var i=0; i<drumButtons.length; i++){
 function handleClick() {
   var buttonName = this.getInnerHTML();
   playAudio(buttonName);
+  animateButton(buttonName);
 }
 
 document.addEventListener("keydown", handleKeyboardPress);
 
 function handleKeyboardPress(e) {
-  console.log(e);
   var buttonName = e.key;
   playAudio(buttonName);
+  animateButton(buttonName);
 }
 
 function playAudio(buttonName) {
   var audioPath = "sounds/";
   switch (buttonName) {
     case "w":
-      audioPath += "snare.mp3"
+      audioPath += "snare.mp3";
       break;
     case "a":
-      audioPath += "tom-1.mp3"
+      audioPath += "tom-1.mp3";
       break;
     case "s":
-      audioPath += "tom-4.mp3"
+      audioPath += "tom-4.mp3";
       break;
     case "d":
-      audioPath += "kick-bass.mp3"
+      audioPath += "kick-bass.mp3";
       break;
     case "j":
-      audioPath += "tom-3.mp3"
+      audioPath += "tom-3.mp3";
       break;
     case "k":
-      audioPath += "tom-2.mp3"
+      audioPath += "tom-2.mp3";
       break;
     case "l":
-      audioPath += "crash.mp3"
+      audioPath += "crash.mp3";
       break;
     default:
+      console.log("Wrong Key pressed!!!!");
+      return;
   }
   var audio = new Audio(audioPath);
   audio.play();
+}
+
+function animateButton(buttonName) {
+  var activeButton = document.querySelector("." + buttonName);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
