6f7ebfdc23527d5ca991a7dd9868169169995509
Yashsaraf96 <sarafyash2015@gmail.com>
2022-07-12 00:41:31 +0530

    Added function to handle user click.

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -9,3 +9,25 @@ function nextSequence(){
   $("div."+randomColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomColor);
 }
+
+$("div.btn").click(handleUserClick);
+
+function handleUserClick(){
+  var userColor = event.path[0].id;
+  userPattern.push(userColor);
+  playSound(userColor);
+  animatePress(userColor);
+  console.log(userPattern);
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(name){
+  $("div#"+ name).addClass("pressed");
+  setTimeout(function(){
+    $("div#"+ name).removeClass("pressed");
+  },100);
+}
