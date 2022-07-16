64b70eb54fdcdece2aa6e2ff170b0f4ba579cc9c
Yashsaraf96 <sarafyash2015@gmail.com>
2022-07-16 23:26:26 +0530

    Added post request handling with user input city name.

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=a4a407d9dd4f6153b529fe7c65a2322a&units=metric";
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  const cityName = req.body.city;
+  const apiKey = "a4a407d9dd4f6153b529fe7c65a2322a";
+  const units = "metric";
+  // This API currently only supports US city names.
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=" + units;
   https.get(url, function(response){
     console.log(response.statusCode);
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const desc = weatherData.weather[0].description;
-      const cityName = weatherData.name;
-      const icon = weatherData.weather[0].icon;
-      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.write("<h1>The temperature in " + cityName + " is " + temp + " degree celcius.</h1>");
-      res.write("<p>The wather is currently " + desc + "</p>");
-      res.write("<img src=" + imageUrl+ ">");
-      res.send();
-    });
+    if (response.statusCode != 200) {
+      res.send("Please Enter a US city name.");
+    }else{
+      response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const desc = weatherData.weather[0].description;
+        const cityName = weatherData.name;
+        const icon = weatherData.weather[0].icon;
+        const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        res.write("<h1>The temperature in " + cityName + " is " + temp + " degree celcius.</h1>");
+        res.write("<p>The wather is currently " + desc + "</p>");
+        res.write("<img src=" + imageUrl+ ">");
+        res.send();
+      });
+    }
   });
 });
 
